96da94142407f90c16a41ffc34613dd57ebb0b89
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-17 04:01:24 +0000

    5

---
+++ task_05.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" true or false"""
+
+
+def defaults(my_required, my_optional=True):
+    """True or False"""
+    return my_optional is my_required
