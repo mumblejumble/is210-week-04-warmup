192b66a5580e29d7ee0830ee656b70421dc2868e
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-18 02:27:40 +0000

    5

--- task_05.py
+++ task_05.py
@@ -4,5 +4,26 @@
 
 
 def defaults(my_required, my_optional=True):
-    """True or False"""
+    """True or False.
+
+    compares if they are identical.
+
+    Args:
+        my_required(bool): random string.
+        my_optional(bool): random string with default True.
+
+    Returns:
+        whether booleans assigned to arguments are identical.
+
+    Examples:
+
+        >>>defaults(True)
+        True
+
+        >>>defaults(True, False)
+        False
+
+        >>>defaults(True, True)
+        True
+    """
     return my_optional is my_required
